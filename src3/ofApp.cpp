c4067a1a43dc0f20f0878853f0deb5c9887aee9e
vasilis <agios.mur@gmail.com>
2017-05-20 13:41:48 +0300

    ofAppCPP-update

--- src3/ofApp.cpp
+++ src3/ofApp.cpp
@@ -113,56 +113,7 @@ void ofApp::drawWithModel(){
 
 //draw the model manually
 void ofApp::drawWithMesh(){
-/*
-	//get the model attributes we need
-	ofVec3f scale = model.getScale();
-	ofVec3f position = model.getPosition();
-	float normalizedScale = model.getNormalizedScale();
-	ofVboMesh mesh = model.getMesh(0);
-	ofTexture texture;
-    ofxAssimpMeshHelper& meshHelper = model.getMeshHelper( 0 );
-    bool bHasTexture = meshHelper.hasTexture();
-    if( bHasTexture ) {
-        texture = model.getTextureForMesh(0);
-    }
 
-	ofMaterial material = model.getMaterialForMesh(0);
-
-    ofPushMatrix();
-
-	//translate and scale based on the positioning.
-	ofTranslate(position);
-	ofRotate(-ofGetMouseX(), 0, 1, 0);
-	ofRotate(90,1,0,0);
-
-
-	ofScale(normalizedScale, normalizedScale, normalizedScale);
-//	ofScale(scale.x,scale.y,scale.z);
-
-	//modify mesh with some noise
-	float liquidness = 5;
-	//float amplitude = mouseY/100.0;
-    float amplitudem.getArgAsFloat(0)
-    cout << amplitude << endl;
-	float speedDampen = 5;
-	vector<ofVec3f>& verts = mesh.getVertices();
-	for(unsigned int i = 0; i < verts.size(); i++){
-		verts[i].x += ofSignedNoise(verts[i].x/liquidness, verts[i].y/liquidness,verts[i].z/liquidness, ofGetElapsedTimef()/speedDampen)*amplitude;
-		verts[i].y += ofSignedNoise(verts[i].z/liquidness, verts[i].x/liquidness,verts[i].y/liquidness, ofGetElapsedTimef()/speedDampen)*amplitude;
-		verts[i].z += ofSignedNoise(verts[i].y/liquidness, verts[i].z/liquidness,verts[i].x/liquidness, ofGetElapsedTimef()/speedDampen)*amplitude;
-	}
-
-	//draw the model manually
-	if(bHasTexture) texture.bind();
-	material.begin();
-	//mesh.drawWireframe(); //you can draw wireframe too
-	mesh.drawFaces();
-	material.end();
-	if(bHasTexture) texture.unbind();
-
-	ofPopMatrix();
-
-    */
     while (receiver.hasWaitingMessages()) {
         ofxOscMessage m;
         receiver.getNextMessage(m);
